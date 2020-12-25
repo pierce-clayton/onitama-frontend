8cea3c9461637c695c2ea5c00db4ef43df417533
Clayton Pierce <46853031+technicholy@users.noreply.github.com>
2020-12-25 13:13:23 -0800

    adds actioncable basics

--- src/containers/Game.js
+++ src/containers/Game.js
@@ -1,6 +1,19 @@
 import React, { Component } from "react";
-
+// import { ActionCable } from 'react-actioncable-provider';
+import { API_ROOT } from '../constants';
+import Cable from '../components/Cable';
 class Game extends Component {
+  state = {
+    games: [],
+    activeGame: null
+  };
+
+  componentDidMount = () => {
+    fetch(`${API_ROOT}/games`)
+    .then(r => r.json())
+    .then(games => this.setState({games}))
+  }
+
   render() {
     return <div>Game!</div>;
   }
--- src/index.js
+++ src/index.js
@@ -3,8 +3,17 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { ActionCableProvider } from 'react-actioncable-provider';
+import { API_WS_ROOT } from "./constants"
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+<ActionCableProvider url={API_WS_ROOT}>
+  <App />
+</ActionCableProvider>,
+
+document.getElementById("root")
+
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
